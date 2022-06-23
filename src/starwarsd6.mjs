300017db5a0e7e2df2e476744ada5816dd8fdb36
Erik <99984933+DarthBanjo@users.noreply.github.com>
2022-06-23 16:40:50 -0400

    Update starwarsd6.mjs

--- src/starwarsd6.mjs
+++ src/starwarsd6.mjs
@@ -97,40 +97,40 @@ class ConfigureOpenD6Options extends FormApplication {
 		await game.settings.set('od6s', 'custom_field_1', game.i18n.localize('starwarsd6.custom_field_1'));
 		await game.settings.set('od6s', 'custom_field_1_short', game.i18n.localize('starwarsd6.custom_field_1_short'));
 		await game.settings.set('od6s', 'custom_field_1_type', game.i18n.localize('starwarsd6.custom_field_1_type'));
-		await game.settings.set('od6s', 'custom_field_1_actor_types', 3));
+		await game.settings.set('od6s', 'custom_field_1_actor_types', "3"));
 	}
 
 	async updateDeadly() {
-		await game.settings.set('od6s', 'deadliness', 3);
-		await game.settings.set('od6s', 'npc-deadliness', 3);
-		await game.settings.set('od6s', 'creature-deadliness', 3);
+		await game.settings.set('od6s', 'deadliness', "3");
+		await game.settings.set('od6s', 'npc-deadliness', "3");
+		await game.settings.set('od6s', 'creature-deadliness', "3");
 	}
 
 	async updateRules() {
-		await game.settings.set('od6s', 'hide_advantages_disadvantages', true);
-		await game.settings.set('od6s', 'hide_compendia', true);
-		await game.settings.set('od6s', 'bodypoints', 0);
-		await game.settings.set('od6s', 'highhitdamage', false);
+		await game.settings.set('od6s', 'hide_advantages_disadvantages', "true");
+		await game.settings.set('od6s', 'hide_compendia', "true");
+		await game.settings.set('od6s', 'bodypoints', "0");
+		await game.settings.set('od6s', 'highhitdamage', "false");
 		await game.settings.set('od6s', 'brawl_attribute', game.i18n.localize('starwarsd6.brawl_attribute'));
-		await game.settings.set('od6s', 'parry_skills', true);
-		await game.settings.set('od6s', 'reaction_skills', true);
-		await game.settings.set('od6s', 'defense_lock', true);
-		await game.settings.set('od6s', 'fate_point_round', true);
-		await game.settings.set('od6s', 'fate_point_climactic', true);
-		await game.settings.set('od6s', 'strength_damage', true);
-		await game.settings.set('od6s', 'metaphysics_attribute_optional', true);
-		await game.settings.set('od6s', 'dice_for_scale', true);
-		await game.settings.set('od6s', 'sensors', true);
-		await game.settings.set('od6s', 'vehicle_difficulty', true);
-		await game.settings.set('od6s', 'passenger_damage_dice', true);
-		await game.settings.set('od6s', 'show_skill_specialization', true);
-		await game.settings.set('od6s', 'dice_for_grenades', true);
-		await game.settings.set('od6s', 'map_range_to_difficulty', true);
-		await game.settings.set('od6s', 'melee_difficulty', true);
-		await game.settings.set('od6s', 'random_hit_locations', true);
-		await game.settings.set('od6s', 'pip_per_dice', 3);
-		await game.settings.set('od6s', 'flat_skills', false);
-		await game.settings.set('od6s', 'pip_per_dice', 3);
+		await game.settings.set('od6s', 'parry_skills', "true");
+		await game.settings.set('od6s', 'reaction_skills', "true");
+		await game.settings.set('od6s', 'defense_lock', "true");
+		await game.settings.set('od6s', 'fate_point_round', "true");
+		await game.settings.set('od6s', 'fate_point_climactic', "true");
+		await game.settings.set('od6s', 'strength_damage', "true");
+		await game.settings.set('od6s', 'metaphysics_attribute_optional', "true");
+		await game.settings.set('od6s', 'dice_for_scale', "true");
+		await game.settings.set('od6s', 'sensors', "true");
+		await game.settings.set('od6s', 'vehicle_difficulty', "true");
+		await game.settings.set('od6s', 'passenger_damage_dice', "true");
+		await game.settings.set('od6s', 'show_skill_specialization', "true");
+		await game.settings.set('od6s', 'dice_for_grenades', "true");
+		await game.settings.set('od6s', 'map_range_to_difficulty', "true");
+		await game.settings.set('od6s', 'melee_difficulty', "true");
+		await game.settings.set('od6s', 'random_hit_locations', "true");
+		await game.settings.set('od6s', 'pip_per_dice', "3");
+		await game.settings.set('od6s', 'flat_skills', "false");
+		await game.settings.set('od6s', 'pip_per_dice', "3");
 	}
 
 	/*
