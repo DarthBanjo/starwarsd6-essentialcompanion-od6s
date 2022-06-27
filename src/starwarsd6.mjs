98352c62bf7f9dab904c024bb19161e89a6eb7b5
DarthBanjo <99984933+DarthBanjo@users.noreply.github.com>
2022-06-27 09:31:20 -0400

    Update starwarsd6.mjs

--- src/starwarsd6.mjs
+++ src/starwarsd6.mjs
@@ -47,7 +47,8 @@ class ConfigureOpenD6Options extends FormApplication {
 				{ id: 'labels', on: state.labels ?? true, desc: 'starwarsd6.form.labels' },
 				{ id: 'fields', on: state.fields ?? true, desc: 'starwarsd6.form.fields' },
 				{ id: 'deadly', on: state.deadly ?? true, desc: 'starwarsd6.form.deadly' },
-				{ id: 'rules', on: state.rules ?? true, desc: 'starwarsd6.form.rules' }
+				{ id: 'rules', on: state.rules ?? true, desc: 'starwarsd6.form.rules' },
+				{ id: 'rules1e', on: state.rules1e ?? true, desc: 'starwarsd6.form.rules1e' }
 			]
 		};
 	}
@@ -65,6 +66,7 @@ class ConfigureOpenD6Options extends FormApplication {
 		if (formData.fields) await this.updateFields();
 		if (formData.deadly) await this.updateDeadly();
 		if (formData.rules) await this.updateRules();
+		if (formData.rules1e) await this.updateRules1e();
 	}
 
 	async updateLabels() {
@@ -131,6 +133,10 @@ class ConfigureOpenD6Options extends FormApplication {
 		await game.settings.set('od6s', 'pip_per_dice', 3);
 		await game.settings.set('od6s', 'flat_skills', false);
 		await game.settings.set('od6s', 'pip_per_dice', 3);
+	}
+	
+	async updateRules1e() {
+		await game.settings.set('od6s', 'use_wild_die', false);
 	}
 
 	/*
